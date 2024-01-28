a8be06ae67e67a9e6cca6447eff1d8b1e3d48011
Rohit Yadav <rk4740779@gmail.com>
2024-01-28 18:27:56 +0530

    done 20

--- Array/20. rearrange elements.cpp
+++ Array/20. rearrange elements.cpp
@@ -23,3 +23,40 @@ public:
 // ! ===============================================================================================
 // * Type 2 :
 // ? https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/
+
+class Solution{
+public: 
+	void rearrange(int arr[], int n) {
+	    vector<int>pos, neg;
+	    
+	    for(int i=0; i<n; i++){
+	        if(arr[i]<0){
+	            neg.push_back(arr[i]);
+	        }else{
+	            pos.push_back(arr[i]);
+	        }
+	    }
+	    
+	    if(pos.size()>neg.size()){ 
+	        for(int i=0; i<neg.size(); i++){
+	            arr[2*i] = pos[i];
+	            arr[2*i+1] = neg[i]; 
+	        }
+	        int index = neg.size()*2;
+	        for(int i=neg.size(); i<pos.size(); i++){
+	            arr[index] = pos[i];
+	            index++;
+	        }
+	    }else{ 
+	        for(int i=0; i<pos.size(); i++){
+	            arr[2*i] = pos[i];
+	            arr[2*i+1] = neg[i]; 
+	        }
+	        int index= pos.size()*2;
+	        for(int i=pos.size(); i<neg.size(); i++){
+	            arr[index] = neg[i];
+	            index++;
+	        } 
+	    }
+	}
+};
