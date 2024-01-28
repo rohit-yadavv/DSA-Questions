f6269df17c7014ad3f77e6f51e85e24b7fc30a08
Rohit Yadav <rk4740779@gmail.com>
2024-01-28 18:28:50 +0530

    done 21

---
+++ Array/21. subarray with sum k.cpp
@@ -0,0 +1,20 @@
+// ? https://leetcode.com/problems/subarray-sum-equals-k/description/ 
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> preSumsMap;
+        preSumsMap[0]++;
+        int count=0;
+        int preSum=0;
+
+        for(int i=0; i<nums.size(); i++){
+            preSum=preSum+nums[i]; 
+            int requiredSum=preSum-k;
+
+            count += preSumsMap[requiredSum];
+            preSumsMap[preSum]++;   
+        }
+        return count;
+    }
+};
