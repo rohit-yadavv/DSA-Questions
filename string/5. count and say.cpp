fae1cb881d98b02c5ef1477eebef9ba4f9cc413d
Rohit Yadav <rk4740779@gmail.com>
2024-02-08 19:56:25 +0530

    done question 4 and 6 of string

---
+++ string/5. count and say.cpp
@@ -0,0 +1,23 @@
+//? https://leetcode.com/problems/count-and-say/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+        for (int i = 2; i <= n; i++){
+            string lastString = ans;
+            int l = ans.size();
+            int j = 0;
+            ans = "";
+            while (j < l){
+                int count = j;
+                while (j < l && lastString[j] == lastString[count]){
+                    count++;
+                }
+                ans += to_string(count - j) + lastString[j];
+                j = count; 
+            }
+        }
+        return ans;
+    } 
+};
