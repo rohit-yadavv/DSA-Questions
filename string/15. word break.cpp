616fdc3aa47dfd44cc1dc228bead494b8cc942e3
Rohit Yadav <rk4740779@gmail.com>
2024-03-08 16:47:19 +0530

    renamed files

---
+++ string/15. word break.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    unordered_set<string> st;
+    int n;
+    int dp[301];
+
+    bool solve(int idx, string& s){
+        if(idx==n){
+            return true;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        if(st.find(s)!=st.end()){
+            return true;
+        }
+        for(int l=0; l<=n; l++){
+            string temp=s.substr(idx, l);
+            if(st.find(temp)!=st.end() && solve(idx+l, s)){
+                return dp[idx]=true;
+            }
+        }
+        return dp[idx]= false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n = s.size();
+
+        memset(dp, -1, sizeof(dp));
+        
+        for(string&word: wordDict){
+            st.insert(word);
+        }
+
+        return solve(0, s);
+    }
+};
