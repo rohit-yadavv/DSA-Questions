94ab857ffc9f5ffcf6ba77ef3372fb3c3763b2ad
Rohit Yadav <rk4740779@gmail.com>
2024-04-10 07:37:25 +0530

    done 25

---
+++ string/25. longest common prefix.cpp
@@ -0,0 +1,22 @@
+//! https://leetcode.com/problems/longest-common-prefix/submissions/1201491322/
+
+class Solution
+{
+public:
+  string longestCommonPrefix(vector<string> &v)
+  {
+    string ans = "";
+    sort(v.begin(), v.end());
+    int n = v.size();
+    string first = v[0], last = v[n - 1];
+    for (int i = 0; i < min(first.size(), last.size()); i++)
+    {
+      if (first[i] != last[i])
+      {
+        return ans;
+      }
+      ans += first[i];
+    }
+    return ans;
+  }
+};
