afbf7c8e79a9824ac0fbc9665388ea9ee384844c
Rohit Yadav <rk4740779@gmail.com>
2024-04-11 16:53:11 +0530

    k

---
+++ string/28.longest subseq.cpp
@@ -0,0 +1,35 @@
+// ? https://leetcode.com/problems/longest-common-subsequence/description/
+
+class Solution
+{
+public:
+  int solve(string &a, string &b, int i, int j, vector<vector<int>> &dp)
+  {
+
+    if (i == a.size())
+      return 0;
+
+    if (j == b.size())
+      return 0;
+
+    if (dp[i][j] != -1)
+      return dp[i][j];
+    int ans = 0;
+    if (a[i] == b[j])
+    {
+      ans = 1 + solve(a, b, i + 1, j + 1, dp);
+    }
+    else
+    {
+      ans = max(solve(a, b, i + 1, j, dp), solve(a, b, i, j + 1, dp));
+    }
+    dp[i][j] = ans;
+    return dp[i][j];
+  }
+
+  int longestCommonSubsequence(string text1, string text2)
+  {
+    vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+    return solve(text1, text2, 0, 0, dp);
+  }
+};
