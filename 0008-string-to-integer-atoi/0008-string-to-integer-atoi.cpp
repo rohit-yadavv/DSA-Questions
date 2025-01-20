87f0890b0772d12b01708cb6a91d4a6d7f0d5a17
Rohit Yadav <126584837+rohit-yadavv@users.noreply.github.com>
2025-01-20 08:12:54 +0530

    Time: 3 ms (16.22%), Space: 10.2 MB (5.26%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,30 +1,33 @@
 class Solution {
+    long atoi(string s, int sign, int i, long result) {
+        if(sign*result>=INT_MAX){
+            return INT_MAX;
+        }
+        if(sign*result<=INT_MIN){
+            return INT_MIN;
+        }
+        if(i>=s.size()|| s[i]<'0' || s[i]>'9'){
+            return sign*result;
+        }
+        
+       
+        result=atoi(s,sign,i+1,(result*10+(s[i]-'0')));
+        
+        return result;
+    }
+
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
-            i++;
-        if(s[i]=='-'){
-            sign=-1;
-            i++;
-        }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.size()){
-            if(s[i]>='0' && s[i]<='9'){
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
-        }
-        return (ans*sign);
+    int myAtoi(string s) {
+        
+        int i = 0, n = s.size(), sign = 1;
+        while (i < n && s[i] == ' ')
+            ++i;
+
+        if (s[i] == '-')
+            sign = -1, ++i;
+        else if (s[i] == '+')
+            ++i;
+
+        return atoi(s, sign, i, 0);
     }
 };
